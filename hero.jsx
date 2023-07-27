a0088860ed046e35685b1e7205aec9f799ab51f1
Raj1128 <118893926+Raj1128@users.noreply.github.com>
2023-07-27 17:55:24 +0530

    Add files via upload

---
+++ hero.jsx
@@ -0,0 +1,35 @@
+
+
+const Hero = () => {
+    return (
+        <main className="hero">
+            <div className="hero-content">
+                <h1>YOUR DESERVE THE BEST</h1>
+            <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+            <div className="hero-btn">
+                <button>Shop now</button>
+                <button>Category</button>
+            </div>
+            <div className="Shopping">
+                <p>Also available on</p>
+                <div className="brand-icons">
+                    <img src="/images/amazon.png" alt="logo" />
+                    <img src="/images/flipkart.png" alt="logo" />
+                </div>
+            </div>
+            </div> 
+            <div className="hero-image">
+                <img src="/images/shoe_image.png" alt="" />
+            </div>
+        </main>
+    )
+
+};
+
+export default Hero;
+
+
+
+
+
+
